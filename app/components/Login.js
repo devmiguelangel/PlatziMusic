88c420ccf6b60a2fb053de37c9789983cb3497b1
miguel <mikeangelodj@gmail.com>
2018-02-27 16:57:06 -0400

    RN - Firebase Authentication

--- app/components/Login.js
+++ app/components/Login.js
@@ -17,20 +17,76 @@ import FBSDK, {
   AccessToken
 } from 'react-native-fbsdk';
 
+import * as firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyA_vfJn9MNrqJ9j0PA3y4GbmiJEz3eWlZ8",
+  authDomain: "platzimusic-196421.firebaseapp.com",
+  databaseURL: "https://platzimusic-196421.firebaseio.com",
+  projectId: "platzimusic-196421",
+  storageBucket: "platzimusic-196421.appspot.com",
+  messagingSenderId: "1057440979740"
+};
+firebase.initializeApp(config);
+
+const { FacebookAuthProvider } = firebase.auth;
+const firebaseAuth = firebase.auth()
+
 export default class Login extends Component {
-  // 
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null
+    }
+  }
+
+  componentWillMount = () => {
+    this.authenticateUSer();
+  }
+
+  authenticateUSer = () => {
+    AccessToken.getCurrentAccessToken().then((data) => {
+      const { accessToken } = data;
+      const credential = FacebookAuthProvider.credential(accessToken);
+
+      firebaseAuth.signInWithCredential(credential).then((user) => {
+        this.setState({ user: user })
+        
+        this.props.navigation.navigate('ArtistList')
+        // console.warn("Sign In Success", user);
+        
+        // var currentUser = user;
+        // Merge prevUser and currentUser data stored in Firebase.
+        // Note: How you handle this is specific to your application
+
+        // After data is migrated delete the duplicate user
+        /* return user.delete().then(function () {
+          // Link the OAuth Credential to original account
+          return prevUser.link(credential);
+        }).then(function () {
+          // Sign in with the newly linked credential
+          return firebaseAuth.signInWithCredential(credential);
+        }); */
+      }).catch((error) => {
+        // console.warn("Sign In Error", error);
+      });
+    });
+  }
+
   handleLogin = (error, result) => {
     if (error) {
       console.warn("login has error: " + result.error);
     } else if (result.isCancelled) {
       alert("login is cancelled.");
     } else {
-      AccessToken.getCurrentAccessToken().then(() => {
+      this.authenticateUSer();
+      /* AccessToken.getCurrentAccessToken().then((data) => {
+        this.authenticateUSer(data.accessToken)
         this.props.navigation.navigate('ArtistList')
-        /* (data) => {
+        (data) => {
           alert(data.accessToken.toString())
-        } */
-      })
+        }
+      }) */
     }
   }
 
@@ -40,6 +96,11 @@ export default class Login extends Component {
         <Text style={styles.welcome}>
           Bienvenidos a PlatziMusic
         </Text>
+        {/*
+        <Text style={styles.welcome}>
+          {this.state.user && this.state.user.displayName}
+        </Text>
+        */}
 
         <LoginButton
           readPermissions={["public_profile", "email"]}
